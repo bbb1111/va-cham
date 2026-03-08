2d8a9027fcd055c42440df100445fd3074f5ed9a
minhnhat <97241416+bbb1111@users.noreply.github.com>
2026-03-08 23:32:17 +0700

    Add touch event support for drawing lines

--- add_line.js
+++ add_line.js
@@ -7,14 +7,40 @@ noClickZone.addEventListener('mouseup', (e) => {
   e.stopPropagation(); // Ngăn sự kiện thả chuột thấm lên window
 });
 
+noClickZone.addEventListener('touchstart', (e) => {
+  e.stopPropagation(); // Ngăn sự kiện thấm lên window
+});
+
+noClickZone.addEventListener('touchend', (e) => {
+  e.stopPropagation(); // Ngăn sự kiện thả chuột thấm lên window
+});
+
 const handleMouseDown = (e) => {
-  x1 = e.clientX;
-  y1 = e.clientY;
+  let clientX , clientY
+  if (e.type.includes('touch')){
+    clientX = e.touches[0].clientX
+    clientY = e.touches[0].clientY
+  }
+  else{
+      clientX = e.clientX
+      clientY = e.clientY    
+  }
+  x1 = clientX;
+  y1 = clientY;
   console.log("Điểm bắt đầu (x1, y1):", x1, y1);
 };
 const handleMouseUp = (e) => {
-  x2 = e.clientX;
-  y2 = e.clientY;
+  let clientX , clientY
+  if (e.type.includes('touch')){
+    clientX = e.touches[0].clientX
+    clientY = e.touches[0].clientY
+  }
+  else{
+      clientX = e.clientX
+      clientY = e.clientY    
+  }
+  x2 = clientX;
+  y2 = clientY;
   console.log("Điểm kết thúc (x2, y2):", x2, y2);
   let vector_ab = [x2 -x1 ,y2 -y1]
   item_list.push({
@@ -32,14 +58,22 @@ toggleBtn.addEventListener('change', function() {
   if (this.checked) {
 
     console.log("Chế độ vẽ: ON");
-    window.addEventListener('pointerdown', handleMouseDown);
-    window.addEventListener('pointerup', handleMouseUp);
+
+    window.removeEventListener('touchstart', handleMouseDown);
+    window.removeEventListener('touchend', handleMouseUp);
+
+    window.addEventListener('mousedown', handleMouseDown);
+    window.addEventListener('mouseup', handleMouseUp);
     } else {
     console.log("Chế độ vẽ: OFF");
     // Quan trọng: Gỡ bỏ sự kiện khi tắt nút
     console.log(item_list)
-    window.removeEventListener('pointerdown', handleMouseDown);
-    window.removeEventListener('pointerup', handleMouseUp);
+
+    window.removeEventListener('touchstart', handleMouseDown);
+    window.removeEventListener('touchend', handleMouseUp);
+
+    window.removeEventListener('mousedown', handleMouseDown);
+    window.removeEventListener('mouseup', handleMouseUp);
 
   }
 });
